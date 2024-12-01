9e288df459ee10ea898d17a99418ffd05a83a098
Denisson Pereira Santos <denisson.pereira753@gmail.com>
2024-12-01 12:30:32 -0300

    Update signIn.tsx

--- src/screens/signIn.tsx
+++ src/screens/signIn.tsx
@@ -1,57 +1,113 @@
-import { StatusBar } from 'expo-status-bar';
-import { Image, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import { Header } from '../components/header';
+import { Alert, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { TitleCustoms } from '../customs/titleCustoms';
 import { colors } from '../colors';
-import { useEffect, useState } from 'react';
-import { IMovies } from '../models/IMovies';
-import { getMoviesNowPlaying } from '../services/getMoviesNowPlaying';
-import { LinearGradient } from 'expo-linear-gradient';
-import { FontAwesome } from '@expo/vector-icons'; 
+import { Entypo, EvilIcons, FontAwesome } from "@expo/vector-icons";
+import { useState } from 'react';
+import { useNavigate } from '../hooks/useNavigate';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../services/firebaseConnection';
 
-export default function Home() {
-  const [movies, setMovies] = useState<IMovies[]>([]);
+export default function SignIn() {
+  const { navigate } = useNavigate();
+  const [passwordType, setPasswordType] = useState<boolean>(true);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-  useEffect(() => {
-    async function fetchData() {
-      const response = await getMoviesNowPlaying();
-      setMovies(response);
+  const seePassword = () => {
+    if (passwordType) {
+      setPasswordType(false);
+    } else {
+      setPasswordType(true);
     }
-    fetchData();
-  }, []);
-
-  if (movies.length === 0) {
-    return <Text>Loading...</Text>;
   }
+
+  const handleSignIn = async () => {
+    if (email === null || password === null) {
+      Alert.alert('Error', 'Please fill in all fields before proceeding.');
+      return;
+    }
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      Alert.alert('Sucess', 'Login successful!');
+      navigate('signIn');
+    } catch (error: any) {
+      Alert.alert('Error', error.message);
+    }
+  }
+
 
   return (
     <View style={styles.container}>
-      <Header />
-      <View style={styles.primeContainer}>
-        <Image
-          source={{ uri: `https://image.tmdb.org/t/p/original/${movies[0].poster_path}` }}
-          style={styles.image}
-        />
-        <LinearGradient
-          colors={[
-            'rgba(0, 0, 0, 0.048)', 
-            'rgba(0, 0, 0, 0.493)',  
-            'rgba(0, 0, 0, 0.658)',  
-            'rgba(0, 0, 0, 0.842)',  
-            colors.bg,              
-          ]}
-          style={styles.shadow}
-        />
-        <View style={styles.info}>
-          <Text style={styles.movieTitle}>{movies[0].title}</Text>
-          <View style={styles.ratingContainer}>
-            <FontAwesome name="star" size={18} color="black" style={styles.star} />
-            <Text style={styles.voteAverage}>{movies[0].vote_average}</Text>
-          </View>
-          <Text style={styles.releaseDate}>{movies[0].release_date}</Text>
-          <TouchableOpacity style={styles.button} onPress={() => alert('See details pressed')}>
-            <Text style={styles.buttonText}>See Details</Text>
+      <TitleCustoms
+        title1='Movie'
+        title2='zone'
+        size={35}
+        weight='700'
+      />
+      <View style={styles.containerTxt}>
+        <Text style={styles.back}>Welcome Back!</Text>
+        <View style={styles.containerTxt2}>
+          <Text style={styles.txt}>Please sign in to your acconut to</Text>
+          <Text style={styles.txt}>continue</Text>
+        </View>
+      </View>
+      <View style={styles.inputContainer}>
+        <View style={styles.input}>
+          <TextInput
+            placeholder='E-mail'
+            placeholderTextColor={colors.grey}
+            style={{ fontSize: 18, color: colors.ice, width: '90%' }}
+            value={email}
+            onChangeText={setEmail}
+          />
+        </View>
+        <View style={styles.input}>
+          <TextInput
+            placeholder='Password'
+            placeholderTextColor={colors.grey}
+            style={{ fontSize: 18, color: colors.ice, width: '90%' }}
+            value={password}
+            onChangeText={setPassword}
+            secureTextEntry={passwordType}
+          />
+          <Entypo
+            name={passwordType ? 'eye-with-line' : 'eye'}
+            color={colors.grey}
+            onPress={seePassword}
+            size={20}
+          />
+        </View>
+      </View>
+      <View style={styles.viewContainer}>
+        <TouchableOpacity
+          style={styles.btn}
+          onPress={handleSignIn}
+        >
+          <Text style={styles.txtBtn}>Sign In</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.orContainer}>
+        <View style={styles.line}></View>
+        <Text style={styles.txt}>Or sign in with</Text>
+        <View style={styles.line}></View>
+      </View>
+      <View style={styles.iconsContainer}>
+        <View>
+          <TouchableOpacity style={styles.iconContainer}>
+            <EvilIcons name='sc-facebook' color='black' size={35} />
           </TouchableOpacity>
         </View>
+        <View>
+          <TouchableOpacity style={styles.iconContainer}>
+            <FontAwesome name='google' color='black' size={25} />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <View style={styles.registerContainer}>
+        <Text style={styles.txt2}>Not registered yet? </Text>
+        <TouchableOpacity onPress={() => navigate('signUp')}>
+          <Text style={styles.txtRed}>Sign Up</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -61,83 +117,96 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: colors.bg,
-    paddingTop: 50,
-    paddingHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    gap: 50
+  },
+  back: {
+    color: colors.ice,
+    fontSize: 28,
+    fontWeight: '600'
+  },
+  containerTxt: {
+    justifyContent: 'center',
+    alignItems: 'center',
     gap: 30
   },
-  primeContainer: {
+  containerTxt2: {
     justifyContent: 'center',
     alignItems: 'center',
-    position: 'relative',
   },
-  image: {
-    width: "100%",
-    height: 500,
-    borderTopLeftRadius: 50,
-    borderTopRightRadius: 50,
-  },
-  shadow: {
-    position: 'absolute',
-    top: '50%',
-    left: 0,
-    right: 0,
-    bottom: 0,
-  },
-  info: {
-    position: 'absolute',
-    bottom: 50,
-    flexDirection: 'column',
-    justifyContent: 'center',
-    width: '100%',
-    paddingHorizontal: 20,
-    alignItems: 'center',
-    gap: 10
-  },
-  movieTitle: {
+  txt: {
     color: colors.ice,
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 10,
+    fontSize: 15,
+    letterSpacing: 1,
+    fontWeight: '300'
   },
-  ratingContainer: {
+  input: {
+    backgroundColor: colors.inputs,
+    width: 380,
+    height: 60,
+    borderRadius: 15,
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+  },
+  inputContainer: {
+    gap: 20
+  },
+  viewContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  btn: {
+    backgroundColor: colors.orange,
+    paddingHorizontal: 50,
+    paddingVertical: 12,
+    borderRadius: 10
+  },
+  txtBtn: {
+    color: colors.white,
+    fontSize: 17,
+    fontWeight: '200'
+  },
+  orContainer: {
     flexDirection: 'row',
     alignItems: 'center',
-    backgroundColor: 'white',
-    paddingVertical: 5,
-    paddingHorizontal: 10,
-    borderRadius: 20,
-    shadowColor: '#000',
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
+    gap: 5
   },
-  star: {
-    marginRight: 5,
+  line: {
+    height: 1,
+    backgroundColor: colors.grey,
+    width: 120
   },
-  voteAverage: {
+  iconsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 30
+  },
+  iconContainer: {
+    backgroundColor: colors.ice,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 13,
+    width: 60,
+    height: 60,
+  },
+  registerContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  txt2: {
+    color: colors.ice,
     fontSize: 15,
-    fontWeight: 'bold',
-    color: 'black',
+    letterSpacing: 1,
+    fontWeight: '500'
   },
-  releaseDate: {
-    fontSize: 16,
-    color: colors.ice,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginBottom: 20, // Dando um espaçamento para o botão ficar abaixo
-  },
-  button: {
-    backgroundColor: colors.bg,
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-    borderRadius: 30,
-    borderWidth: 1,
-    borderColor: colors.ice,
-  },
-  buttonText: {
-    color: colors.ice,
-    fontSize: 16,
-    fontWeight: 'bold',
-  },
+  txtRed: {
+    color: colors.red,
+    fontSize: 15,
+    letterSpacing: 1,
+    fontWeight: '500'
+  }
 });
