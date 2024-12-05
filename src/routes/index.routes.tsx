d8e69d90e309e4c8ccdf6a7ee4864e2988b7b439
wendellbruno <79750052+wendellbruno@users.noreply.github.com>
2024-12-05 11:36:23 -0200

    alguns ajustes
    alguns ajustes

--- src/routes/index.routes.tsx
+++ src/routes/index.routes.tsx
@@ -2,20 +2,21 @@ import { createStackNavigator } from "@react-navigation/stack";
 import { useAuth } from "../context";
 import { NavigationContainer } from "@react-navigation/native";
 import { TabRoutes } from "./tab.routes";
-import Initial from "../screens/initial";
-import SignIn from "../screens/signIn";
-import SignUp from "../screens/signUp";
-import MovieDetails from "../screens/movieDetails";
+import Initial from '../screens/Initial';
+import SignIn from "../screens/SignIn";
+import SignUp from "../screens/SignUp";
+import MovieDetails from "../screens/MovieDetails";
 
 const Stack = createStackNavigator();
 
 export default function Routes() {
     const { user } = useAuth();
+    console.log(user);
 
     return (
         <NavigationContainer>
             <Stack.Navigator screenOptions={{ headerShown: false }}>
-                {user ? (
+                {user && user !== null || user && user !== undefined ?  (
                     <>
                         <Stack.Screen name="tabViews" component={TabRoutes} />
                         <Stack.Screen name="movieDetails" component={MovieDetails} />
